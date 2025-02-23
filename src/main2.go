1c96dc0213063e6995597e4a3a4e1816b9432f11
Grant Herrema <granther321@gmail.com>
2025-02-23 18:49:09 -0500

    Trying out different methods

--- src/main2.go
+++ src/main2.go
@@ -10,9 +10,12 @@ import (
 )
 
 func main() {
-	if err := textSocket(webSock()); err != nil {
-		panic(err)	
-	}
+	ws := webSock()
+	//err := textSocket(ws); err != nil {
+	//	panic(err)	
+	//}
+
+	go listenWS(ws)
 
 	if handle, err := pcap.OpenLive("lo", 1600, true, pcap.BlockForever); err != nil {
 	  panic(err)
@@ -22,14 +25,24 @@ func main() {
 	  packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	  for packet := range packetSource.Packets() {
 		fmt.Println("packet: ", packet)
+		ws.Write(packet.Data())
 	  }
 	}
 }
 
+func listenWS(ws *websocket.Conn) {
+	var msg = make([]byte, 512)
+	for {
+		_, err := ws.Read(msg)
+		if err != nil { fmt.Println("Error reading from websocket"); continue }
+		fmt.Println("Got message from server on websocket")
+	}
+}
+
 func webSock() *websocket.Conn {
-	//ws, err := websocket.Dial("wss://192.168.1.41/wg", "", "http://192.168.1.68/")
+	ws, err := websocket.Dial("ws://192.168.1.41:8080/ws", "", "http://192.168.1.68/")
 	fmt.Println("Dialing websocker")
-	ws, err := websocket.Dial("wss://localhost:80/wg", "", "http://localhost/")
+	//ws, err := websocket.Dial("ws://localhost:8080/ws", "", "http://localhost/")
 	if err != nil {
 		fmt.Println("WebSocket error:", err)
 		return nil
@@ -49,6 +62,6 @@ func textSocket(ws *websocket.Conn) error {
 	if n, err = ws.Read(msg); err != nil {
 		return err
 	}
-	fmt.Println("Read message: ", msg[:n])
+	fmt.Println("Read message: ", string(msg[:n]))
 	return nil
 }
